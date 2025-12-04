1963343b33b49c54489e313775eb575ae27645bf
hmock <hmock@ycds.org>
2025-12-04 00:32:49 -0500

    Finish red close

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -14,28 +15,39 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(120, 120, Math.toRadians(180), Math.toRadians(180), 17.3)
                 .build();
-
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 14, Math.toRadians(0)))
+//        myBot.setDimensions()
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, 15, Math.toRadians(0)))
                 //shoot 3
 
 
 
-//                .splineTo(new Vector2d(50, 36), Math.toRadians(180))
 
-//                .strafeTo(new Vector2d(60,36))
-//                .lineToX()
-                        .strafeTo(new Vector2d(55,36))
-                                .turn(Math.toRadians(90))
-                                .strafeTo(new Vector2d( 58,36))
-//                .turn(Math.toRadians(90))
-//                .splineTo(new Vector2d(60, 42), Math.toRadians(0))
+//                .splineToSplineHeading(
+//                        new Pose2d(30, 20, Math.toRadians(90)),
+//                        Math.toRadians(90) // arriving toward +X
+//                )
 
-//                .lineToY(30)
-//                .turn(Math.toRadians(90))
-//                .lineToX(0)
-//                .turn(Math.toRadians(90))
-//                .lineToY(0)
-//                .turn(Math.toRadians(90))
+
+//                .splineToLinearHeading(new Pose2d(-52, 38, Math.toRadians(90)), Math.toRadians(200))
+
+
+
+//                .splineToLinearHeading(new Pose2d(50,13, Math.toRadians(0)), Math.toRadians(0))
+//                .strafeTo(new Vector2d(50,15))
+                .strafeTo(new Vector2d(55,15))
+                .splineToLinearHeading(new Pose2d(50,15,Math.toRadians(155)), Math.toRadians(0))
+//                .waitSeconds(3)
+                .splineToLinearHeading(new Pose2d(55,18,Math.toRadians(155)),Math.toRadians(0))
+                .waitSeconds(3)//SHOOT 3 HERE
+                .splineToLinearHeading(new Pose2d(58, 36, Math.toRadians(90)), Math.toRadians(0))
+                .strafeTo(new Vector2d(60,36))
+
+
+
+
+//                        .strafeTo(new Vector2d(55,36))
+//                                .turn(Math.toRadians(90))
+//                                .strafeTo(new Vector2d( 58,36))
                 .build());
 
 
