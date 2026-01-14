a1e6c4e02edc552e7a5f0d25e1f54ffb7f6ea9f6
James Moscola <jmoscola@ycp.edu>
2026-01-13 21:49:59 -0500

    cleaned up the Red_Far trajectories and actions a bit more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Red_Far.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Red_Far.java
@@ -4,6 +4,8 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.InstantFunction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -17,56 +19,69 @@ import org.firstinspires.ftc.teamcode.Utilities.PoseStorage;
 @Autonomous(name = "Red_Far")
 public class Red_Far extends LinearOpMode {
 
-    private final GearhoundsHardware robot = new GearhoundsHardware();
-    MecanumDrive drive;
-
-    // Starting pose
-    Pose2d startPose = new Pose2d(new Vector2d(60, 15), Math.toRadians(180));
-
-
     @Override
     public void runOpMode() throws InterruptedException {
-        // Initialize robot (done ONCE)
-        robot.init(hardwareMap);
+        // **************************************************
+        // initialize hardware
+        // **************************************************
+        GearhoundsHardware robot = new GearhoundsHardware();
+        robot.init(hardwareMap);  // Initialize robot (done ONCE)
 
-        // Create drive AFTER hardwareMap is ready
-        drive = new MecanumDrive(hardwareMap, startPose);
 
-        // Create drive AFTER hardwareMap is ready
+        // **************************************************
+        // initialize hardware mechanisms
+        // **************************************************
+        Pose2d startPose = new Pose2d(new Vector2d(60, 15), Math.toRadians(180));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
         Shooter shooter = new Shooter(robot);
 
+
+        // **************************************************
+        // define required trajectories
+        // **************************************************
+        TrajectoryActionBuilder gotoShootingPositionBuilder = drive.actionBuilder(startPose)
+                .waitSeconds(10)
+                .strafeToLinearHeading(new Vector2d(55, 15), Math.toRadians(163));
+
+        // the following builder that includes 'endTrajectory.fresh' should always come at the end of a trajectory
+        // so any new trajectories start fresh from the latest pose
+        TrajectoryActionBuilder endGotoShootingPositionBuilder = gotoShootingPositionBuilder.endTrajectory().fresh();
+
+
+        // **************************************************
+        // build required Actions from trajectories
+        // **************************************************
+        Action gotoShootingPosition = gotoShootingPositionBuilder.build();
+        Action endGotoShootingPosition = endGotoShootingPositionBuilder.build();
+
+
+        // **************************************************
+        // wait patiently for start
+        // **************************************************
         waitForStart();
-
         if (isStopRequested()) return;
-//        Actions.runBlocking(
-//                drive.actionBuilder(startPose)
-//                        .waitSeconds(10)
-//                        .strafeToLinearHeading(new Vector2d(55, 15), Math.toRadians(163))
-//                        .waitSeconds(3)
-//                        .stopAndAdd(shooter.runShooter(1400, 1400, 0.2))
-//                        .stopAndAdd(shooter.shootBallRapid(3, 1, 4))
-//                        //                        .strafeToLinearHeading(new Vector2d(0,15),Math.toRadians(180))
-//                        .build());
 
+
+        // **************************************************
+        // create and run action sequence
+        // **************************************************
         Actions.runBlocking(new SequentialAction(
-                        // drive.
-//                        SleepAction(10),
-                        // strafe
-//                        SleepAction(10),
+                        gotoShootingPosition,
+                        new SleepAction(3),
                         shooter.runShooter(1400, 1400, 0.2),
-                        shooter.shootBallRapid(3, 1, 4)
+                        shooter.shootBallRapid(3, 1, 4),
+                        endGotoShootingPosition
                 )
-        );
-
+        ); // end Actions.runBlocking action sequence
     }
 
 
     /// Please don't go messing around in here if you don't know what you are doing proceed with CAUTION
 
-    public class SavePose implements InstantFunction {
-        @Override
-        public void run() {
-            PoseStorage.currentPose = drive.localizer.getPose();
-        }
-    }
+//    public class SavePose implements InstantFunction {
+//        @Override
+//        public void run() {
+//            PoseStorage.currentPose = drive.localizer.getPose();
+//        }
+//    }
 }
