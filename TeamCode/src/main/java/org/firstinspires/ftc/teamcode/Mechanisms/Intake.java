91dd8c88a8ac701ad9ca541eb0af19f5a670cdee
hmock <hmock@ycds.org>
2026-01-14 10:48:45 -0500

    Add new intake mechanism
    add runIntake
    add stopIntake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Intake.java
@@ -1,35 +1,84 @@
-//package org.firstinspires.ftc.teamcode.Mechanisms;
-//
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//
-//
-//public class Intake {
-//
-//    private final DcMotorEx intake;
-//
-//    public Intake(final HardwareMap hMap, final String name) {
-//        intake = hMap.get(DcMotorEx.class, "intake");
-//        intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//    }
-//
-//
-//    public double getIntakePower(){
-//        double intakePower = intake.getPower();
-//        return intakePower;
-//    }
-//
-//    public void intakeOff(){
-//        intake.setPower(0);
-//    }
-//
-//    public void intakeOn(){
-//        intake.setPower(1);
-//    }
-//
-//    public void intakeReverse(){
-//        intake.setPower(-1);
-//    }
-//}
+package org.firstinspires.ftc.teamcode.Mechanisms;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Utilities.GearhoundsHardware;
+
+
+public class Intake {
+
+    private final GearhoundsHardware robot;
+
+    public Intake(GearhoundsHardware robot) {
+        this.robot = robot;
+    }
+
+    public Action runIntake(double power, double timeout) {
+        return new runIntake(power, timeout);
+    }
+
+    public Action stopShooter() {
+        return new stopIntake();
+    }
+
+
+
+    public class runIntake implements Action {
+        double power;
+        double timeout;
+        ElapsedTime timer;
+
+
+        public runIntake(double Power, double Timeout) {
+            this.power = Power;
+            this.timeout = Timeout;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if (timer == null) {
+                timer = new ElapsedTime();
+                //put code to initialize here per action
+            }
+            if (power == 0) {
+                robot.leftLight.setPosition(0.28);
+                robot.rightLight.setPosition(0.28);
+                throw new RuntimeException("The intake power can not be 0");
+            }
+            if (power < -1 || power > 1) {
+                robot.leftLight.setPosition(0.28);
+                robot.rightLight.setPosition(0.28);
+                throw new RuntimeException("The intake uses .setPower! Can not be above or below 1 or -1 Change to continue");
+            }
+            if (timeout <= 0) {
+                robot.leftLight.setPosition(0.28);
+                robot.rightLight.setPosition(0.28);
+                throw new RuntimeException("The intake timeout needs to be greater than 0!");
+            }
+            robot.intake.setPower(power);
+
+
+            if (timer.seconds() < timeout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
+    public class stopIntake implements Action {
+        double power = 0;
+        ElapsedTime timer;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            robot.intake.setPower(power);
+            return false;
+        }
+    }
+}
