af5fb75488ceb238a8cd953945ba75616b19329d
cshaffer <cshaffer1@ycds.org>
2026-03-07 09:35:52 -0500

    Red Far 9 Ball added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Red_Far_9_Ball.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Red_Far_9_Ball.java
@@ -1,33 +1,17 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.acmerobotics.roadrunner.InstantFunction;
+import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Pose2dDual;
 import com.acmerobotics.roadrunner.PoseMap;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.Mechanisms.Intake;
-import org.firstinspires.ftc.teamcode.Mechanisms.Shooter;
-import org.firstinspires.ftc.teamcode.Mechanisms.Transfer;
-import org.firstinspires.ftc.teamcode.Utilities.GearhoundsHardware;
-import org.firstinspires.ftc.teamcode.Utilities.PoseStorage;
-
-
-import com.acmerobotics.roadrunner.InstantFunction;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Pose2dDual;
-import com.acmerobotics.roadrunner.PoseMap;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.sun.tools.javac.comp.Todo;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Mechanisms.Intake;
@@ -41,6 +25,9 @@ import org.firstinspires.ftc.teamcode.Utilities.PoseStorage;
 public class Red_Far_9_Ball extends LinearOpMode {
 
     private final GearhoundsHardware robot = new GearhoundsHardware();
+
+    public int topVelocity = 1186;
+    public int bottomVelocity = 1186;
     MecanumDrive drive;
     // Starting pose
     Pose2d startPose = new Pose2d(new Vector2d(60, 15), Math.toRadians(180));
@@ -51,7 +38,7 @@ public class Red_Far_9_Ball extends LinearOpMode {
 You may see the word "Pose thrown around a lot. Pose is essentially just a way of explaining a point in space using coordinates like X and Y
  */
 
-/// Some of the Autonomous commands include (these may change as time goes on will be updated here)
+    /// Some of the Autonomous commands include (these may change as time goes on will be updated here)
 /*
 .stopAndAdd(new SavePose())
 
@@ -75,8 +62,6 @@ You may see the word "Pose thrown around a lot. Pose is essentially just a way o
 //                 )
 //         )
 //)
-
-
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -94,17 +79,84 @@ You may see the word "Pose thrown around a lot. Pose is essentially just a way o
         if (isStopRequested()) return;
         Actions.runBlocking(
                 drive.actionBuilder(startPose)
+                        .strafeToSplineHeading(new Vector2d(55, 14), Math.toRadians(158))
+                        .stopAndAdd(
+                                new ParallelAction(
+                                        shooter.runShooter(topVelocity, bottomVelocity),
+                                        intake.runIntake(1, 1),
+                                        new SequentialAction(
+                                                new SleepAction(1.5),
+                                                transfer.runTransfer(),
+                                                new SleepAction(1),
+                                                transfer.stopTransfer(),
+                                                shooter.stopShooter()
+                                        )
+                                )
+                        )
+                        .strafeToSplineHeading(new Vector2d(35, 25), Math.toRadians(90))
+                        .strafeToConstantHeading(new Vector2d(35, 60))
+                        .stopAndAdd(transfer.tapTransfer())
+                        .strafeToSplineHeading(new Vector2d(55, 14), Math.toRadians(158))
+                        .stopAndAdd(
+                                new ParallelAction(
+                                        shooter.runShooter(topVelocity, bottomVelocity),
+                                        new SequentialAction(
+                                                new SleepAction(1),
+                                                transfer.runTransfer(),
+                                                new SleepAction(1),
+                                                transfer.stopTransfer(),
+                                                shooter.stopShooter()
+                                        )
+                                )
+                        )
+                        .waitSeconds(0.5)
+                        .strafeToSplineHeading(new Vector2d(12, 25), Math.toRadians(90))
+                        .strafeToConstantHeading(new Vector2d(12, 60))
+                        .stopAndAdd(transfer.tapTransfer())
+                        .strafeToSplineHeading(new Vector2d(55, 14), Math.toRadians(158))
+                        .stopAndAdd(
+                                new ParallelAction(
+                                        shooter.runShooter(topVelocity + 10, bottomVelocity + 10),
+                                        intake.runIntake(1, 1),
+                                        new SequentialAction(
+                                                new SleepAction(1),
+                                                transfer.runTransfer(),
+                                                new SleepAction(1),
+                                                transfer.stopTransfer(),
+                                                shooter.stopShooter()
+                                        )
+                                )
+                        )
+                        .splineToSplineHeading(new Pose2d(60, 52.5, Math.toRadians(90)), Math.toRadians(0))
+                        .strafeTo(new Vector2d(60, 60))
+                        .stopAndAdd(transfer.tapTransfer())
+                        .strafeToSplineHeading(new Vector2d(55, 14), Math.toRadians(158))
+                        .stopAndAdd(
+                                new ParallelAction(
+                                        shooter.runShooter(topVelocity, bottomVelocity),
+                                        intake.runIntake(1, 1),
+                                        new SequentialAction(
+                                                new SleepAction(1),
+                                                transfer.runTransfer(),
+                                                new SleepAction(1),
+                                                transfer.stopTransfer(),
+                                                shooter.stopShooter(),
+                                                intake.stopIntake()
+                                        )
+                                )
+                        )
+                        .strafeTo(new Vector2d(35, 14))
+
+
                         .stopAndAdd(new SavePose())
+
                         .build());
 
 
     }
 
 
-
-
     public class SavePose implements InstantFunction {
-        @Override
         public void run() {
             PoseStorage.currentPose = drive.localizer.getPose();
         }
